cdec218dc2a4c85c353dd864ffb20e8ff41ffbfd
1503013 <1503013@nchc.org.tw>
2021-02-22 11:58:00 +0800

    test update commit

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# noinspection SpellCheckingInspection
 def print_hi(nametest):
-    # Use a breakpoint in the code line below to debug your script.
+
     print(f'Hi, {nametest}')  # Press ⌘F8 to toggle the breakpoint.
 
 
